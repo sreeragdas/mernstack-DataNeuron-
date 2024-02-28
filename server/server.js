894cb3ba28524d124529afa2fefc6ba8852a58c5
sreeragdas <you@example.com>
2024-02-28 09:10:23 +0530

    cors issue resolved

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 const corsOptions ={
-    origin:'https://65dea6dfcb3576879317ea8e--tubular-fairy-b8f431.netlify.app/', 
+    origin:'https://tubular-fairy-b8f431.netlify.app/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
